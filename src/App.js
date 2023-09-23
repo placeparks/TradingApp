4ec7b78db9ae34089055d9099c7dea62da100c70
placeparks <placeparks@gmail.com>
2023-09-23 17:06:28 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,98 +1,106 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import React, { useState } from 'react';
+import { Modal, Button, Tab, Tabs, Form } from 'react-bootstrap';
+import { Web3Button } from "@thirdweb-dev/react";
+import { contractAddress, contractAbi } from "./contract";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles/Home.css";
+import LottieLoader from 'react-lottie-loader';
+import trade from './assets/trade.json';
 
 export default function Home() {
+  const [amountPILA, setAmountPILA] = useState(0);
+  const [amountUSDT, setAmountUSDT] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [activeTab, setActiveTab] = useState('buy');
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+  const roundToDecimals = (number, decimals) => {
+    const factor = Math.pow(10, decimals);
+    return Math.round(number * factor) / factor;
+  };
+
+  const calculateReceivedUSDTForSelling = (value) => {
+    const conversionRate = 2.00e-7;
+    return roundToDecimals(value * conversionRate, 7);  // Assuming 7 decimal places for USDT
+  };
+
+  const convertToSmallestUnitForBuy = (value) => {
+    return value * Math.pow(10, 6);
+  };
+
+  const convertToSmallestUnitForSell = (value) => {
+    return value * Math.pow(10, 6);
+  };
+
+  const calculateRequiredUSDT = (value) => {
+    return (value / 5);
+  };
+
+
+
   return (
     <main className="main">
       <div className="container">
-        <div className="header">
-          <h1 className="title">
-            Welcome to{" "}
-            <span className="gradient-text-0">
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className="description">
-            Get started by configuring your desired network in{" "}
-            <code className="code">src/index.js</code>, then modify the{" "}
-            <code className="code">src/App.js</code> file!
-          </p>
-
-          <div className="connect">
-            <ConnectWallet
-              dropdownPosition={{
-                side: "bottom",
-                align: "center",
-              }}
-            />
-          </div>
-        </div>
-
-        <div className="grid">
-          <a
-            href="https://portal.thirdweb.com/"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-1">Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-2">Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-3">Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
+      <h1>Welcome to the PILA Exchange Platform!</h1>
+        <p>
+          You can buy PILA tokens using USDT. Not only that,
+          but you can also convert your PILA tokens back to USDT whenever you want.
+        </p>
+        
+        <Button onClick={handleShow}>Buy/Sell</Button>
+        <LottieLoader animationData={trade} style={{ height: '400px' }} />
+        <Modal show={showModal} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{activeTab === 'buy' ? 'Buy PILA' : 'Sell PILA'}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Tabs activeKey={activeTab} onSelect={(k) => setActiveTab(k)}>
+              <Tab eventKey="buy" title="Buy">
+                <Form.Group>
+                  <Form.Label>Enter amount of PILA</Form.Label>
+                  <Form.Control 
+                    type="number"
+                    value={amountPILA}
+                    onChange={(e) => {
+                      setAmountPILA(e.target.value);
+                      setAmountUSDT(calculateRequiredUSDT(e.target.value));
+                    }}
+                    placeholder="Enter amount of PILA"
+                  />
+                  <Form.Text>Required USDT: {amountUSDT}</Form.Text>
+                </Form.Group>
+                <Web3Button
+                  contractAddress={contractAddress}
+                  contractAbi={contractAbi}
+                  action={(contract) => contract.call("BuyPilaWithUSDT", [convertToSmallestUnitForBuy(amountUSDT)])}
+                >
+                  Buy PILA
+                </Web3Button>
+              </Tab>
+              <Tab eventKey="sell" title="Sell">
+                <Form.Group>
+                  <Form.Label>Enter amount of PILA to sell</Form.Label>
+                  <Form.Control 
+                    type="number"
+                    value={amountPILA}
+                    onChange={(e) => setAmountPILA(e.target.value)}
+                    placeholder="Enter amount of PILA to sell"
+                  />
+                  <Form.Text>You will receive approximately {calculateReceivedUSDTForSelling(amountPILA).toFixed(7)} USDT for selling {amountPILA} PILA</Form.Text>
+                </Form.Group>
+                <Web3Button
+          contractAddress={contractAddress}
+          contractAbi={contractAbi}
+          action={(contract) => contract.call("SellPilaForUSDT", [amountPILA])}
+        >
+          Sell PILA
+        </Web3Button>
+              </Tab>
+            </Tabs>
+          </Modal.Body>
+        </Modal>
       </div>
     </main>
   );
